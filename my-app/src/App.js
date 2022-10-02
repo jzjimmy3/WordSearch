ad3e78bd3555da8088abf0437b0dc4b421af7fdc
Jimmy Zhang <jzjimmy3@gmail.com>
2022-10-02 16:36:22 -0400

    Initial Commit
    Creating WordSearch App

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <h1> Word Search</h1>
+      </nav>
+
+      <Board />
+      <Keyboard />
+    </div>
+  );
+}
+
+export default App;
