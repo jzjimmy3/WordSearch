19b62554f01d68f490db9975997d5395111c80ad
Jimmy Zhang <jzjimmy3@gmail.com>
2022-10-02 18:19:35 -0400

    Basic CSS styling

--- my-app/src/components/Board.js
+++ my-app/src/components/Board.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
+// import Letter from './Letter';
 
 function Board() {
     return (
-        <div>
-            <h1>Board</h1>
+        <div className="board">
+            <div className="row">1</div>
+            <div className="row">2</div>
+            <div className="row">3</div>
+            <div className="row">4</div>
+            <div className="row">5</div>
         </div>
+
     )
 }
 
