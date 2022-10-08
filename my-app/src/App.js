c1c9f081c859577a1d7e98702abd1c2ebb897d07
Jimmy Zhang <jzjimmy3@gmail.com>
2022-10-07 21:23:15 -0400

    Randomized Letter in Grid

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,7 @@
-import './App.css';
-import Board from './components/Board';
-import Keyboard from './components/Keyboard';
-import Grid from './components/Grid';
-import SearchBar from './components/SearchBar';
+import "./App.css";
+import Keyboard from "./components/Keyboard";
+import Grid from "./components/Grid";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
@@ -11,11 +10,11 @@ function App() {
         <h1> Word Search</h1>
       </nav>
       <div className="board " id="grid-container">
-        {Grid(50, 50)}
+        {Grid(10, 10)}
       </div>
       <SearchBar />
       <Keyboard />
-    </div >
+    </div>
   );
 }
 
--- my-app/src/components/Grid.js
+++ my-app/src/components/Grid.js
@@ -1,18 +1,21 @@
 import React from "react";
-import '../App.css';
-import { useRef, useEffect } from 'react';
+import "../App.css";
+import { useRef, useEffect } from "react";
 
 export default function Grid(rows, cols) {
-    const ref = useRef(null);
-    useEffect(() => {
-        const container = document.getElementById("grid-container");
-        container.style.setProperty("--grid-cols", cols);
-        container.style.setProperty("--grid-rows", rows);
-        var count = 1;
-        for (var i = 0; i < (rows * cols); i++) {
-            console.log(count++);
-            const grid = document.createElement('div');
-            container.appendChild(grid).className = "grid-item";
-        };
-    })
+  const gridLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  const ref = useRef(null);
+  useEffect(() => {
+    const container = document.getElementById("grid-container");
+    container.style.setProperty("--grid-cols", cols);
+    container.style.setProperty("--grid-rows", rows);
+    for (var i = 0; i < rows * cols; i++) {
+      const grid = document.createElement("div");
+      const text = (grid.textContent = gridLetters.charAt(
+        Math.floor(Math.random() * 26)
+      ));
+      container.appendChild(grid).className = "grid-item";
+    }
+  });
 }
